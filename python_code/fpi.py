3f5143f2bf8c9c0df4dae15f65ee78e4bd5b8521
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-07-22 11:13:49 -0500

    PEP8

--- python_code/fpi.py
+++ python_code/fpi.py
@@ -2,12 +2,14 @@
 # Author: John Stachurski
 # Date: August 2009
 # Corresponds to: Listing 6.6
+from scipy import absolute as abs
 
 from fvi import *  # Import all definitions from listing 6.5
-from scipy import absolute as abs
+
 
 def maximizer(h, a, b):
     return float(fminbound(lambda x: -h(x), a, b))
+
 
 def T(sigma, w):
     "Implements the operator L T_sigma."
@@ -17,6 +19,7 @@ def T(sigma, w):
         vals.append(Tw_y)
     return LinInterp(grid, vals)
 
+
 def get_greedy(w):
     "Computes a w-greedy policy."
     vals = []
@@ -24,6 +27,7 @@ def get_greedy(w):
         h = lambda k: U(y - k) + rho * mean(w(f(k, W)))
         vals.append(maximizer(h, 0, y))
     return LinInterp(grid, vals)
+
 
 def get_value(sigma, v):
     """Computes an approximation to v_sigma, the value
